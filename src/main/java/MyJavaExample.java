2af3393b7d6435a95e623082e4a2355c0907b7d7
Andy An <anandy@synopsys.com>
2024-05-28 09:33:04 -0600

    initial commit

---
+++ src/main/java/MyJavaExample.java
@@ -0,0 +1,30 @@
+public class MyJavaExample {
+    public static void main(String[] args) {
+        int loops = 100;
+        System.out.print(getTotal(loops));
+    }
+
+    private static int getTotal(int loops) {
+        int total = 0;
+        while (loops > 0) {
+            total += loops;
+        }
+        return total;
+    }
+    
+    
+ 
+    
+    public static class QueryClauses {
+        public String fromClause;
+        public String whereClause;
+        public String havingClause;
+
+
+        public QueryClauses(String fromClause, String whereClause, String havingClause) {
+            this.fromClause = fromClause;
+            this.whereClause = whereClause;
+            this.havingClause = havingClause;
+        }
+    }
+}
