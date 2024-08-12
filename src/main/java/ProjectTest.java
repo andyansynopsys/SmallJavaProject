4f8c141249bf882c61478f26de085246882bc3cb
Andy An <anandy@synopsys.com>
2024-08-12 11:50:20 -0600

    add a change

---
+++ src/main/java/ProjectTest.java
@@ -0,0 +1,20 @@
+public class ProjectTest {
+    public ProjectTest() {
+
+    }
+
+    public void test() {
+        int loops = 100;
+        int total = getTotal(loops);
+        System.out.print(total);
+    }
+
+    private int getTotal(int loops) {
+        int total = 0;
+        while (loops > 0) {
+            total += loops;
+        }
+        return total;
+    }
+}
+
